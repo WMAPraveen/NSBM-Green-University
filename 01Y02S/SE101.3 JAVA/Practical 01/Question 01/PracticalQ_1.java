7f076ffd6af05eba0a67364e3f943c46722103e9
WMAPraveen <wmapraveen@students.nsbm.ac.lk>
2023-07-04 13:42:32 +0530

    update

---
+++ 01Y02S/SE101.3 JAVA/Practical 01/Question 01/PracticalQ_1.java
@@ -0,0 +1,14 @@
+
+//1.Write you first java program to display “Hello World” on the screen. 
+
+package com.mycompany.practicalq_1;
+
+public class PracticalQ_1 
+{
+        public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
+
+
