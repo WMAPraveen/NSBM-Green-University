d20b4cdce39075debde6ff3d8aeeebdb7ac8c5d5
WMAPraveen <wmapraveen@students.nsbm.ac.lk>
2023-07-05 15:07:14 +0530

    update

---
+++ 01Y02S/SE101.3 JAVA/Practical 01/Question 02/PracticalQ2.java
@@ -0,0 +1,11 @@
+package com.mycompany.practicalq2;
+
+public class PracticalQ2 {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Avishka Wijesinghe");
+        System.out.println("BSc(Hons)Software Engineering");
+
+    }
+}
